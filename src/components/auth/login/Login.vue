7bbc354d73d68aa2bd8b296b5a55d85026849a23
yashgandhi-32 <yashgandhi32@gmail.com>
2018-04-11 08:48:15 +0530

    component structure changed

---
+++ src/components/auth/login/Login.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div class="wrapper">
+
+        <!-- <img src="" alt="Logo"/> -->
+
+        <!-- <div class="fbLogin">FB login button here</div>  -->
+
+        <!-- <p style="margin-bottom:0px">OR</p> -->
+   
+        <form action="">
+
+            <input type="text" id="email" v-model="user.username" placeholder="Email" class="email" />
+            <input type="password" v-model="user.password" placeholder="Password" id="password" class="password" />
+     
+            <div class="signupButtons">
+                <button @click.prevent ="login" id="login">Log in</button>
+                <button id="signup">Sign up</button>
+            </div>
+
+        </form>
+        
+     </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", this.user);
+    }
+  }
+};
+</script>
+ 
+<style scoped>
+  @import url("./login.css");
+</style>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 import Home from '../components/home/Home.vue'
-import Login from "../components/auth/Login.vue"
+import Login from "../components/auth/login/Login.vue"
 
 export const routes = [
+    { path: '/login', component: Login },
     {
         path: '/', component: Home,
         children: [
-            { path: 'login', component: Login },
+           
         ]
     },
 ]
